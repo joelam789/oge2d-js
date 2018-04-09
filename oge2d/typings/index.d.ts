f9315f0dd1b2c3654deb620364324b4f42b54ac3
joelam789 <joelam789@yahoo.com.hk>
2018-04-09 18:05:52 +0800

    update pixi.js to v4.7.3

--- oge2d/typings/index.d.ts
+++ oge2d/typings/index.d.ts
@@ -1,6 +1,3 @@
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/createjs-lib/index.d.ts" />
 /// <reference path="globals/pixi-layers/index.d.ts" />
 /// <reference path="globals/pixi.js/index.d.ts" />
-/// <reference path="globals/tweenjs/index.d.ts" />
 /// <reference path="globals/zip.js/index.d.ts" />
